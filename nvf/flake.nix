a1942b73108ed8a2b644b900a55bf2778cc4c2d7
isushilpuri <isushilpuri@gmail.com>
2025-08-21 23:18:40 +0530

    Init nvf config

---
+++ nvf/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nvf.url = "github:notashelf/nvf";
+  };
+
+  outputs = { self, nixpkgs, nvf, ... }: {
+    packages.x86_64-linux.default = 
+    	(nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./nvf-configuration.nix ];
+        }).neovim;
+  };
+}

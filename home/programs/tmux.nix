d29b5219ac54c65a45a5e1b95e02e5e7dd78d8a3
isushilpuri <isushilpuri@gmail.com>
2025-08-23 18:14:07 +0530

    Home Gen 2025-08-23 18:14 : id 15 -> /nix/store/bww25vd890cp74s6fdp2khb3kvl2a0jx-home-manager-generation (current)

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   programs.tmux = {
@@ -7,7 +7,21 @@
     escapeTime = 2000;
     mouse = true;
     sensibleOnTop = true;
-    prefix = "M-Space";
+    prefix = "C-Space";
+    extraConfig = ''
+      # Shift Alt vim keys to switch windows
+      bind -n M-H previous-window
+      bind -n M-L next-window
+      
+      # keybindings
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel 
+      
+      # Open panes in current directory
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+    '';
   };
 }
 

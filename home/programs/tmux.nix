8439447ec9786a63af6be2f03c07fc1b73fba418
isushilpuri <isushilpuri@gmail.com>
2025-08-23 19:32:38 +0530

    Home Gen 2025-08-23 19:32 : id 21 -> /nix/store/54cjsa2wdp1s0066wq2f26bsijni1bv3-home-manager-generation (current)

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -18,16 +18,23 @@
       # Shift Alt vim keys to switch windows
       bind -n M-H previous-window
       bind -n M-L next-window
-      
+
       # keybindings
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-selection
-      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel 
-      
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
       # Open panes in current directory
       bind '"' split-window -v -c "#{pane_current_path}"
       bind % split-window -h -c "#{pane_current_path}"
     '';
+    plugins = with pkgs; [
+      {
+        plugin = tmuxPlugins.gruvbox;
+        extraConfig = ''
+          set -g @tmux-gruvbox 'dark'
+        '';
+      }
+    ];
   };
 }
-

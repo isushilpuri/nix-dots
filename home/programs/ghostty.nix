eae84d277600a379f4859e06a12a44abc102c7e0
isushilpuri <isushilpuri@gmail.com>
2025-08-26 23:26:47 +0530

    Home Gen 2025-08-26 23:24 : id 46 -> /nix/store/h8mz9q30cgj78m2pgqycg8zqqalw6z8g-home-manager-generation (current)

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  programs.ghostty = {
-    enable = true;
-    installVimSyntax = true;
-    enableZshIntegration = true;
+    programs.ghostty = {
+	enable = true;
+	installVimSyntax = true;
+	enableZshIntegration = true;
+	settings = {
+	    font-family = "IntoneMono Nerd Font";
+	};
   };
 }
 

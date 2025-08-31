7fe4090b7407fda0684fb3ef9a32895ea02bf8e0
isushilpuri <isushilpuri@gmail.com>
2025-08-31 22:57:30 +0530

    Home Gen 2025-08-31 22:57 : id 53 -> /nix/store/1vjl0pjsw9q4ms2kd9m7bsylmkx4v77c-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -30,6 +30,7 @@
     zig
     gimp
     unzip
+    foliate
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

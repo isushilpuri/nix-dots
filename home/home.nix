0b67d5fad3095ed4e71059ff9f793d0818a8bf7e
isushilpuri <isushilpuri@gmail.com>
2025-08-31 21:50:57 +0530

    Home Gen 2025-08-31 21:50 : id 51 -> /nix/store/63z8k0yw7zqxhr4lcid02hp5b3spjlyf-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -30,6 +30,7 @@
     zig
     gimp
     unzip
+    logseq
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the

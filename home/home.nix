948874a1dcd847226f0516e4da02bd062dd2c802
isushilpuri <isushilpuri@gmail.com>
2025-09-21 14:26:32 +0530

    Home Gen 2025-09-21 14:26 : id 65 -> /nix/store/6icw9m0zwd65f4pzl01bldxs16f9zbi8-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -33,6 +33,8 @@
     unzip
     foliate
     qutebrowser
+    keepassxc
+    gnumake
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

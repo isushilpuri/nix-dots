8f458ef7657e7da19acb9ec623e7e41211102841
isushilpuri <isushilpuri@gmail.com>
2025-09-28 23:42:31 +0530

    Home Gen 2025-09-28 23:42 : id 68 -> /nix/store/587xijncq4cgkdhni6cr5fak4f0mi6sh-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -37,6 +37,7 @@
     gnumake
     onlyoffice-desktopeditors
     pandoc
+    python313Packages.pip
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

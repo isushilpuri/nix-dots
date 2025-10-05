e1077110ec6cd64aa86b231156b512157040e22a
isushilpuri <isushilpuri@gmail.com>
2025-10-05 17:24:28 +0530

    Home Gen 2025-10-05 17:24 : id 69 -> /nix/store/srn6zgm7rzrj4gbyg4vpm6inckr8lb9b-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -38,6 +38,7 @@
     onlyoffice-desktopeditors
     pandoc
     python313Packages.pip
+    inkscape
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

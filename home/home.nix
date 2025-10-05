fecb7ea07aad0604b9689bc267b2daa2a9a55cbd
isushilpuri <isushilpuri@gmail.com>
2025-10-05 18:00:19 +0530

    Home Gen 2025-10-05 18:00 : id 70 -> /nix/store/3mfna2ni2k8949bpxkgk1av4mc4cj3xm-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -39,6 +39,7 @@
     pandoc
     python313Packages.pip
     inkscape
+    scribus
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of

3a4ab662533d3ee9cd016c0c94f8b0f7931b02cb
isushilpuri <isushilpuri@gmail.com>
2025-10-22 14:39:32 +0530

    Home Gen 2025-10-22 14:39 : id 80 -> /nix/store/hvr6h16n8irp2y3238y3vpggb64x6pwz-home-manager-generation (current)

--- home/home.nix
+++ home/home.nix
@@ -43,6 +43,7 @@
     libreoffice
     wineWowPackages.fonts
     yaziPlugins.mount
+    jan
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
